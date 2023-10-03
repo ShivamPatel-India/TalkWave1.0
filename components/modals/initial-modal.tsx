1d698bffa203e1f22d569b049e94c716764745d9
shivam1209 <shivampatel.sp515@gmail.com>
2023-10-02 21:23:40 -0700

    Uploadthing setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -22,6 +22,7 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -60,7 +61,7 @@ export const InitialModal = () => {
         <DialogContent className="bg-white text-black p-0 overflow-hidden">
           <DialogHeader className="pt-8 px-6">
             <DialogTitle className="text-2xl text-center font-bold">
-              customize your server
+              Customize Your Server
             </DialogTitle>
             <DialogDescription className="text-center text-zinc-500">
               Give your server a personality with a name and an image. By the
@@ -71,7 +72,22 @@ export const InitialModal = () => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <div className="space-y-8 px-6">
                 <div className="flex items-center justify-center text-center">
-                  TODO: Image Upload
+                  <FormField 
+                    control={form.control}
+                    name="imageUrl"
+                    render={({field}) => (
+                      <FormItem>
+                        <FormControl>
+                          <FileUpload 
+                            endpoint="serverImage"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  
                 </div>
                 <FormField
                   control={form.control}
