9b75c6747e7377a1f508db241dfbd1fcecd632de
shivam1209 <shivampatel.sp515@gmail.com>
2023-10-08 15:51:58 -0700

    added functionality to edit the name and display picture of the existing servers.

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,31 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorize", { status: 401 });
+    }
+
+    const { name, imageUrl } = await req.json();
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
