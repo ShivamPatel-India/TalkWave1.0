1247a65d1845a8791c51a23e0177ee97ce0de5b7
shivam1209 <shivampatel.sp515@gmail.com>
2023-10-08 14:10:17 -0700

    Added invitations functionality. Admin can generate invitation link for the server and share it to other users. User with invitation link can jon the server as guest member.

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
   UserPlus,
   Users,
 } from "lucide-react";
+import { UseModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -25,6 +26,7 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = UseModal();
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
   return (
@@ -48,6 +50,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       >
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
             className="text-indigo-600 dark:text-indigo-400
                         px-3 py-2 text-sm cursor-pointer"
           >
@@ -75,20 +78,16 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-            <DropdownMenuItem
-                className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
-            >
-                Delete Server
-                <Trash className="h-4 w-4 ml-auto"/>
-            </DropdownMenuItem>
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
         )}
         {!isAdmin && (
-            <DropdownMenuItem
-                className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
-            >
-                Leave Server
-                <LogOut className="h-4 w-4 ml-auto"/>
-            </DropdownMenuItem>
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
         )}
       </DropdownMenuContent>
     </DropdownMenu>
