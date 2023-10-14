2fab0b073417709dc09fc016c583f9211ee558cd
shivam1209 <shivampatel.sp515@gmail.com>
2023-10-14 15:01:11 -0700

    added functionality to leave and delete the server. Both the functions are based on users role. User can delete the server only if he/she is admin.

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -25,7 +25,29 @@ export async function PATCH(
     });
     return NextResponse.json(server);
   } catch (error) {
-    console.log("[SERVER_ID]", error);
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorize", { status: 401 });
+    }
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,24 +1,31 @@
 import { Server } from "@prisma/client";
-import {create} from "zustand";
+import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel";
+export type ModalType =
+  | "createServer"
+  | "invite"
+  | "editServer"
+  | "members"
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer";
 
 interface ModalData {
-    server?: Server
+  server?: Server;
 }
 
 interface ModalStore {
-    type: ModalType | null;
-    data: ModalData;
-    isOpen: boolean;
-    onOpen: (type: ModalType, data?: ModalData) => void;
-    onClose: () => void;
+  type: ModalType | null;
+  data: ModalData;
+  isOpen: boolean;
+  onOpen: (type: ModalType, data?: ModalData) => void;
+  onClose: () => void;
 }
 
 export const UseModal = create<ModalStore>((set) => ({
-    type: null,
-    data: {},
-    isOpen: false,
-    onOpen: (type, data = {}) => set({isOpen: true, type,data}),
-    onClose: () => set({type: null, isOpen: false})
+  type: null,
+  data: {},
+  isOpen: false,
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ type: null, isOpen: false }),
 }));
